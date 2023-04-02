86b7f7a01961b56accf23d53c4e63967ec92bb0d
RITESHKUMARTHAKUR <thakurram2668@gmail.com>
2023-04-02 15:54:18 +0530

    Added Profile Update

--- app/src/main/java/com/example/plants/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/example/plants/fragments/shopping/ProfileFragment.kt
@@ -1,7 +1,30 @@
 package com.example.plants.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.plants.R
+import com.example.plants.databinding.FragmentProfileBinding
 
 class ProfileFragment: Fragment(R.layout.fragment_profile) {
+    private lateinit var binding: FragmentProfileBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val profileBtn = binding.switchButton
+        profileBtn.setOnClickListener{
+            Navigation.findNavController(view).navigate(R.id.action_profileFragment_to_updateFragment)
+        }
+    }
 }
