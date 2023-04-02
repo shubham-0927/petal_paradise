af1242264418083de0b017da7cce4c53eac24bb0
Shubham <shubhamdewangan31@gmail.com>
2023-04-02 18:40:39 +0530

    fix issue with ProfileFragment transition.

--- app/src/main/java/com/example/plants/fragments/shopping/ProfileFragment.kt
+++ app/src/main/java/com/example/plants/fragments/shopping/ProfileFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import androidx.navigation.Navigation
 import com.example.plants.R
 import com.example.plants.databinding.FragmentProfileBinding
+import com.example.plants.fragments.categories.UpdateProfile
 
 class ProfileFragment: Fragment(R.layout.fragment_profile) {
     private lateinit var binding: FragmentProfileBinding
@@ -24,7 +27,13 @@ class ProfileFragment: Fragment(R.layout.fragment_profile) {
         super.onViewCreated(view, savedInstanceState)
         val profileBtn = binding.switchButton
         profileBtn.setOnClickListener{
-            Navigation.findNavController(view).navigate(R.id.action_profileFragment_to_updateFragment)
+//            Navigation.findNavController(view).navigate(R.id.action_profileFragment_to_updateFragment)
+            fragmentManager?.commit {
+                setReorderingAllowed(true)
+                // Replace whatever is in the fragment_container view with this fragment
+                replace<UpdateProfile>(R.id.shoppinHostFragment) //shoppingHostFragment id of frame
+                addToBackStack("to_profile")
+            }
         }
     }
 }
